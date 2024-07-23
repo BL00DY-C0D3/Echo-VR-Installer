e32f01cc783cc2377404a9215b5053fdf18f03ec
BL00DY-C0D3 <github@marceldomain.de>
2024-07-23 18:16:08 +0200

    - Back to the roots: use http instead of torrent - Added a (not yet working) delete cache button - Downloads in http can be continued

---
+++ src/main/java/bl00dy_c0d3_/echovr_installer/DeleteCache.java
@@ -0,0 +1,38 @@
+package bl00dy_c0d3_.echovr_installer;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+public class DeleteCache {
+    // Array of file and folder paths to be deleted
+    private static final String[] pathsToDelete = {
+            "C:/EchoVR/ready-at-dawn-echo-arena.zip",
+            Paths.get(System.getProperty("java.io.tmpdir"), "echo/") + "",
+    };
+
+    public void executeDeletion() {
+        // Loop through each path and attempt to delete the file/folder
+        for (String path : pathsToDelete) {
+            File fileOrDirectory = new File(path);
+            if (deleteFileOrDirectory(fileOrDirectory)) {
+                System.out.println("Deleted: " + fileOrDirectory.getName());
+            } else {
+                System.out.println("Failed to delete: " + fileOrDirectory.getName());
+            }
+        }
+    }
+
+    private boolean deleteFileOrDirectory(File fileOrDirectory) {
+        if (fileOrDirectory.isDirectory()) {
+            // If it's a directory, delete its contents recursively
+            File[] allContents = fileOrDirectory.listFiles();
+            if (allContents != null) {
+                for (File fileContent : allContents) {
+                    deleteFileOrDirectory(fileContent);
+                }
+            }
+        }
+        // Delete the file or the now-empty directory
+        return fileOrDirectory.delete();
+    }
+}
